3e44fb1c2d6ff4d479e3ba3a00b8c7d7e0c5fb5a
Ryan Frizzell <rjfrizz@gmail.com>
2016-05-01 17:25:09 -0400

    creates sidebar layout

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,7 @@
 $( document ).ready(function() {
   //initialize mobile menu
   $(".button-collapse").sideNav();
+
+  //pushpin sidebar
+  $('.sidebar').pushpin({ top: $('.sidebar').offset().top });
 });
